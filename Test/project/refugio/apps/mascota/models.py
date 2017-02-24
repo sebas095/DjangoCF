575c82d403be0e1d22c95579ab0fd5007d6ebdf4
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-02-24 02:10:07 -0500

    update models

--- Test/project/refugio/apps/mascota/models.py
+++ Test/project/refugio/apps/mascota/models.py
@@ -10,4 +10,4 @@ class Mascota(models.Model):
     edad_aproximada = models.IntegerField()
     fecha_rescate = models.DateField()
     persona = models.ForeignKey(Persona, null=True, blank=True, on_delete=models.CASCADE)
-    vacuna = models.ManyToManyField(Vacuna)
+    vacuna = models.ManyToManyField(Vacuna, blank=True)
