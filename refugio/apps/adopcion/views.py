c8f4164a2c9c3bf477852c952fa421bdd0984cf8
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-02-26 14:36:02 -0500

    config urls and first view

--- refugio/apps/adopcion/views.py
+++ refugio/apps/adopcion/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index_adopcion(request):
+    return HttpResponse('Soy la pagina principal de la app adopcion')
+
