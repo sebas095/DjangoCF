313cbf99de6135ad9aba3a6455ca622505b232f3
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-02-27 01:49:04 -0500

    edit and delete

--- refugio/apps/mascota/urls.py
+++ refugio/apps/mascota/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from .views import index, mascota_view, mascota_list
+from .views import index, mascota_view, mascota_list, mascota_edit, mascota_delete
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^nuevo$', mascota_view, name='mascota_crear'),
-    url(r'^listar$', mascota_list, name='mascota_list'),
+    url(r'^nuevo/$', mascota_view, name='mascota_crear'),
+    url(r'^listar/$', mascota_list, name='mascota_list'),
+    url(r'^editar/(?P<id_mascota>\d+)/$', mascota_edit, name='mascota_editar'),
+    url(r'^eliminar/(?P<id_mascota>\d+)/$', mascota_delete, name='mascota_eliminar'),
 ]
--- refugio/apps/mascota/views.py
+++ refugio/apps/mascota/views.py
@@ -18,6 +18,26 @@ def mascota_view(request):
     return render(request, 'mascota/mascota_form.html', {'form': form})
 
 def mascota_list(request):
-    mascota = Mascota.objects.all()
+    mascota = Mascota.objects.all().order_by('id')
     context = {'mascotas': mascota }
     return render(request, 'mascota/mascota_list.html', context)
+
+def mascota_edit(request, id_mascota):
+    mascota = Mascota.objects.get(id=id_mascota)
+    if request.method == 'GET':
+        form = MascotaForm(instance=mascota)
+    else:
+        form = MascotaForm(request.POST, instance=mascota)
+        if form.is_valid():
+            form.save()
+        return redirect('/mascota/listar')
+
+    return render(request, 'mascota/mascota_form.html', {'form': form})
+
+def mascota_delete(request, id_mascota):
+    mascota = Mascota.objects.get(id=id_mascota)
+    if request.method == 'POST':
+        mascota.delete()
+        return redirect('/mascota/listar')
+
+    return render(request, 'mascota/mascota_delete.html', {'mascota': mascota})
