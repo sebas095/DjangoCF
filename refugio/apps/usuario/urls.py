ab05fb4b460047b06cfcfbee65bc30fa9d98c54a
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-02-28 22:41:43 -0500

    Restframework ModelSerializer

--- refugio/apps/usuario/urls.py
+++ refugio/apps/usuario/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import RegistroUsuario
+from .views import RegistroUsuario, UserAPI
 
 urlpatterns = [
     url(r'^registrar', RegistroUsuario.as_view(), name='registrar'),
+    url(r'^api', UserAPI.as_view(), name='api'),
 ]
--- refugio/apps/usuario/views.py
+++ refugio/apps/usuario/views.py
@@ -1,6 +1,10 @@
 from django.views.generic import CreateView
+from django.http import HttpResponse
 from django.core.urlresolvers import reverse_lazy
+from rest_framework.views import APIView
 from .forms import RegistroForm, User
+from .serializers import UserSerializer
+import json
 
 # Create your views here
 class RegistroUsuario(CreateView):
@@ -8,3 +12,11 @@ class RegistroUsuario(CreateView):
     template_name = 'usuario/registrar.html'
     form_class = RegistroForm
     success_url = reverse_lazy('mascota_list')
+
+class UserAPI(APIView):
+    serializer = UserSerializer
+
+    def get(self, request, format=None):
+        lista = User.objects.all()
+        response = self.serializer(lista, many=True)
+        return HttpResponse(json.dumps(response.data), content_type='application/json')
